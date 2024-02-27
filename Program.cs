ac87d0b42c2edbd432123c8e53b81f7ef3c36de3
tudorg <tudorg@indsoft.ro>
2024-02-27 10:09:58 +0200

    Added functionality to display the type and sound for 3 animals

--- Program.cs
+++ Program.cs
@@ -1,10 +1,22 @@
-﻿namespace ConsoleAppAnimalSounds
+﻿using ConsoleAppAnimalSounds.Animals;
+
+namespace ConsoleAppAnimalSounds
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<Animal> animals = new List<Animal>
+            {
+                new Dog(),
+                new Cat(),
+                new Cow()
+            };
+
+            foreach (var animal in animals)
+            {
+                Console.WriteLine($"{animal.Type} makes sound: {animal.MakeSound()}");
+            }
         }
     }
 }
